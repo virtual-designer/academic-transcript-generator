061206a0437174dbd02910c92352ad526a7b35a6
Ar Rakin <rakinar2@osndevs.org>
2026-05-08 20:52:20 +0600

    feat: retake course support
    Signed-off-by: Ar Rakin <rakinar2@osndevs.org>

--- src/atsgen.c
+++ src/atsgen.c
@@ -10,6 +10,7 @@ struct semester_record
     int semester;
     int course_ids[MAX_COURSES];
     int course_marks[MAX_COURSES];
+    int course_ignored_flags[MAX_COURSES];
     int course_count;
 };
 
@@ -124,7 +125,7 @@ void print_course_list(void)
     printf("\n");
 }
 
-void add_courses_interactive(int added_courses[], int added_course_marks[], int *added_course_count)
+void add_courses_interactive(struct semester_record records[], int record_count)
 {
     printf("Courses are listed below,");
     printf(" please enter the appropriate course IDs");
@@ -153,9 +154,9 @@ void add_courses_interactive(int added_courses[], int added_course_marks[], int 
 
         int dup_course = 0;
 
-        for (int i = 0; i < *added_course_count; i++)
+        for (int i = 0; i < records[record_count].course_count; i++)
         {
-            if (added_courses[i] == course_id)
+            if (records[record_count].course_ids[i] == course_id)
             {
                 dup_course = 1;
                 printf("Course already added: [%d] (%s - %s)\n", course_id + 1, course_codes[course_id], course_names[course_id]);
@@ -179,15 +180,27 @@ void add_courses_interactive(int added_courses[], int added_course_marks[], int 
             continue;
         }
 
-        added_courses[*added_course_count] = course_id;
-        (*added_course_count)++;
-        added_course_marks[course_id] = marks;
+        for (int i = 0; i < record_count; i++)
+        {
+            for (int j = 0; j < records[i].course_count; j++)
+            {
+                if (records[i].course_ids[j] == course_id)
+                {
+                    records[i].course_ignored_flags[course_id] = 1;
+                }
+            }
+        }
+
+        records[record_count].course_ids[records[record_count].course_count] = course_id;
+        records[record_count].course_count++;
+        records[record_count].course_marks[course_id] = marks;
+        records[record_count].course_ignored_flags[course_id] = 0;
 
         printf("Course added: %s - %s [Marks: %d/100]\n\n", course_codes[course_id], course_names[course_id], marks);
 
-        if ((*added_course_count) >= MAX_COURSES)
+        if (records[record_count].course_count >= MAX_COURSES)
         {
-            printf("Max course limit exceeded (%d), no more course entries allowed\n", *added_course_count);
+            printf("Max course limit exceeded (%d), no more course entries allowed\n", records[record_count].course_count);
             break;
         }
     }
@@ -211,21 +224,22 @@ void print_transcript(char *name, struct semester_record_list *record_list)
     {
         double term_wgp = 0;
         double term_credits = 0;
+        struct semester_record record = record_list->list[i];
 
-        if (record_list->list[i].course_count < 1)
+        if (record.course_count < 1)
         {
             continue;
         }
 
-        printf("Semester:       %d\n", record_list->list[i].semester);
+        printf("Semester:       %d\n", record.semester);
         printf("\n");
         printf("ID\tCourse");
 
         int max = 0;
 
-        for (int j = 0; j < record_list->list[i].course_count; j++)
+        for (int j = 0; j < record.course_count; j++)
         {
-            int course_id = record_list->list[i].course_ids[j];
+            int course_id = record.course_ids[j];
             int len = (int) strlen(course_codes[course_id]) + (int) strlen(course_names[course_id]);
 
             if (len > max)
@@ -240,40 +254,44 @@ void print_transcript(char *name, struct semester_record_list *record_list)
         printf("Credits  Marks  GP  "
                "  Grade\n");
 
-        for (int j = 0; j < record_list->list[i].course_count; j++)
+        for (int j = 0; j < record.course_count; j++)
         {
-            int course_id = record_list->list[i].course_ids[j];
+            int course_id = record.course_ids[j];
+            int max_len = max - 3 - (int) strlen(course_codes[course_id]) - (int) strlen(course_names[course_id]);
 
             printf("[%d]\t%s - %s", course_id + 1, course_codes[course_id], course_names[course_id]);
 
-            for (int k = 0; k < max - 3 - (int) strlen(course_codes[course_id]) - (int) strlen(course_names[course_id]); k++)
+            for (int k = 0; k < max_len; k++)
                 printf(" ");
 
-            double gp = calc_gp(record_list->list[i].course_marks[course_id]);
+            double gp = calc_gp(record.course_marks[course_id]);
+            char credit_ignored_msg[32] = "        ";
 
-            printf("  %d        %-3d    %1.2lf  %s\n", course_credits[course_id], record_list->list[i].course_marks[course_id], gp, get_letter_grade(gp));
+            if (record.course_ignored_flags[course_id])
+            {
+                strcpy(credit_ignored_msg, " [NC]   ");
+            }
 
-            credits += course_credits[course_id];
-            wgp += gp * course_credits[course_id];
-            term_credits += course_credits[course_id];
-            term_wgp += gp * course_credits[course_id];
+            printf("  %d%s%-3d    %1.2lf  %s\n", course_credits[course_id], credit_ignored_msg, record.course_marks[course_id], gp, get_letter_grade(gp));
+
+            if (!record.course_ignored_flags[course_id])
+            {
+                credits += course_credits[course_id];
+                wgp += gp * course_credits[course_id];
+                term_credits += course_credits[course_id];
+                term_wgp += gp * course_credits[course_id];
+            }
         }
 
         double tgpa = 0.0;
-        double local_cgpa = 0.0;
 
         if (term_credits > 0)
         {
             tgpa = term_wgp / term_credits;
         }
 
-        if (credits > 0)
-        {
-            local_cgpa = wgp / credits;
-        }
-
-        printf("\nTGPA:           %1.2lf (%s)\n", tgpa, get_letter_grade(tgpa));
-        printf("CGPA:           %1.2lf (%s)\n", local_cgpa, get_letter_grade(local_cgpa));
+        printf("\nTerm Credits:   %.0lf\n", term_credits);
+        printf("TGPA:           %1.2lf (%s)\n", tgpa, get_letter_grade(tgpa));
         print_divider();
     }
 
@@ -284,7 +302,8 @@ void print_transcript(char *name, struct semester_record_list *record_list)
         cgpa = wgp / credits;
     }
 
-    printf("Final CGPA:     %1.2lf (%s)\n", cgpa, get_letter_grade(cgpa));
+    printf("Credits:        %.0lf\n", credits);
+    printf("CGPA:           %1.2lf (%s)\n", cgpa, get_letter_grade(cgpa));
     print_divider();
     printf("**** End of transcript ****\n");
 }
@@ -304,6 +323,8 @@ int main(void)
     {
         memset(record_list.list[record_list.count].course_ids, 0, sizeof(int) * MAX_COURSES);
         memset(record_list.list[record_list.count].course_marks, 0, sizeof(int) * MAX_COURSES);
+        memset(record_list.list[record_list.count].course_ignored_flags, 0, sizeof(int) * MAX_COURSES);
+
         record_list.list[record_list.count].course_count = 0;
 
         set_semester(&record_list.list[record_list.count].semester);
@@ -311,8 +332,7 @@ int main(void)
         if (record_list.list[record_list.count].semester == 0)
             break;
 
-        add_courses_interactive(record_list.list[record_list.count].course_ids, record_list.list[record_list.count].course_marks,
-                                &record_list.list[record_list.count].course_count);
+        add_courses_interactive(record_list.list, record_list.count);
 
         record_list.count++;
 
